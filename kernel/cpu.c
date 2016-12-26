591b7491ad3634b88b7edb2647f956eb550c99f4
Thomas Gleixner <tglx@linutronix.de>
2016-12-26 22:58:19 +0100

    smp/hotplug: Undo tglxs brainfart
    The attempt to prevent overwriting an active state resulted in a
    disaster which effectively disables all dynamically allocated hotplug
    states.
    
    Cleanup the mess.
    
    Fixes: dc280d936239 ("cpu/hotplug: Prevent overwriting of callbacks")
    Reported-by: Markus Trippelsdorf <markus@trippelsdorf.de>
    Reported-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1796,6 +1796,7 @@ int __cpuhp_setup_state(enum cpuhp_state state,
 			bool multi_instance)
 {
 	int cpu, ret = 0;
+	bool dynstate;
 
 	if (cpuhp_cb_check(state) || !name)
 		return -EINVAL;
@@ -1807,6 +1808,12 @@ int __cpuhp_setup_state(enum cpuhp_state state,
 
 	ret = cpuhp_store_callbacks(state, name, startup, teardown,
 				    multi_instance);
+
+	dynstate = state == CPUHP_AP_ONLINE_DYN;
+	if (ret > 0 && dynstate) {
+		state = ret;
+		ret = 0;
+	}
 
 	if (ret || !invoke || !startup)
 		goto out;
@@ -1837,7 +1844,7 @@ out:
 	 * If the requested state is CPUHP_AP_ONLINE_DYN, return the
 	 * dynamically allocated state in case of success.
 	 */
-	if (!ret && state == CPUHP_AP_ONLINE_DYN)
+	if (!ret && dynstate)
 		return state;
 	return ret;
 }
