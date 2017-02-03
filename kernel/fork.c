abac83dfd939da13e708176454b0c6cc49dd260b
Ingo Molnar <mingo@kernel.org>
2017-02-03 21:37:49 +0100

    sched/headers, RCU: Move rcu_copy_process() from <linux/sched/task.h> to kernel/fork.c
    Move rcu_copy_process() into kernel/fork.c, which is the only
    user of this inline function.
    
    This simplifies <linux/sched/task.h> to the level that <linux/sched.h>
    does not have to be included in it anymore - which change is done
    in a subsequent patch.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: Ie9b01c38ca031ce7d640575b098d4457faa21da5

--- kernel/fork.c
+++ kernel/fork.c
@@ -1559,6 +1559,19 @@ static int pidfd_create(struct pid *pid)
 		put_pid(pid);
 
 	return fd;
+static inline void rcu_copy_process(struct task_struct *p)
+{
+#ifdef CONFIG_PREEMPT_RCU
+	p->rcu_read_lock_nesting = 0;
+	p->rcu_read_unlock_special.s = 0;
+	p->rcu_blocked_node = NULL;
+	INIT_LIST_HEAD(&p->rcu_node_entry);
+#endif /* #ifdef CONFIG_PREEMPT_RCU */
+#ifdef CONFIG_TASKS_RCU
+	p->rcu_tasks_holdout = false;
+	INIT_LIST_HEAD(&p->rcu_tasks_holdout_list);
+	p->rcu_tasks_idle_cpu = -1;
+#endif /* #ifdef CONFIG_TASKS_RCU */
 }
 
 /*
