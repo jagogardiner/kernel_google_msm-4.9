a34337208983db63504a59edbaf663e5d68e241e
Ingo Molnar <mingo@kernel.org>
2017-02-05 11:48:36 +0100

    sched/headers: Move cputime functionality from <linux/sched.h> and <linux/cputime.h> into <linux/sched/cputime.h>
    Move cputime related functionality out of <linux/sched.h>, as most code
    that includes <linux/sched.h> does not use that functionality.
    
    Move data types that are not included in task_struct directly to
    the signal definitions, into <linux/sched/signal.h>.
    
    Also merge the (small) existing <linux/cputime.h> header into <linux/sched/cputime.h>.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: I6208cea08a5f107f2d731a0dd310cff66bcaf5df

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1961,12 +1961,6 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
 
-<<<<<<< HEAD
-=======
-extern int task_can_switch_user(struct user_struct *up,
-					struct task_struct *tsk);
-
->>>>>>> 70806b21e4d6... sched/headers: Move the 'root_task_group' declaration to <linux/sched/autogroup.h>
 #ifndef TASK_SIZE_OF
 #define TASK_SIZE_OF(tsk)	TASK_SIZE
 #endif
