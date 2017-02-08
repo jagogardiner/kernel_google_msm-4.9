578d1c9ffa43889fc3bcedeed6426e3177a9cb83
Ingo Molnar <mingo@kernel.org>
2017-02-08 18:51:31 +0100

    sched/headers: Prepare for new header dependencies before moving more code to <linux/sched/mm.h>
    We are going to split more MM APIs out of <linux/sched.h>, which
    will have to be picked up from a couple of .c files.
    
    The APIs that we are going to move are:
    
      arch_pick_mmap_layout()
      arch_get_unmapped_area()
      arch_get_unmapped_area_topdown()
      mm_update_next_owner()
    
    Include the header in the files that are going to need it.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: Ic0061c66a503a611bf859b12aa190303961b7a20

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -12,7 +12,6 @@
 #include <linux/sched/numa_balancing.h>
 #include <linux/sched/mm.h>
 #include <linux/sched/cpufreq.h>
-<<<<<<< HEAD
 #include <linux/sched/stat.h>
 #include <linux/sched/nohz.h>
 #include <linux/sched/debug.h>
@@ -23,8 +22,6 @@
 #include <linux/sched/init.h>
 #include <linux/sched/smt.h>
 
-=======
->>>>>>> 55687da166bf... sched/headers: Prepare for new header dependencies before moving code to <linux/sched/cpufreq.h>
 #include <linux/u64_stats_sync.h>
 #include <linux/kernel_stat.h>
 #include <linux/binfmts.h>
