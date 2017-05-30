8a961c6e884f2b281e493474a461be55a1fd914b
Thomas Gleixner <tglx@linutronix.de>
2017-05-30 23:15:41 +0200

    posix-timers: Move posix-timer internals to core
    None of these declarations is required outside of kernel/time. Move them to
    an internal header.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Link: http://lkml.kernel.org/r/20170530211656.394803853@linutronix.de
    Change-Id: Ic991b3f864148cefaf88cea49c76d8be045bf3ce

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -29,6 +29,11 @@
 #include <linux/workqueue.h>
 #include <linux/freezer.h>
 
+#include "posix-timers.h"
+
+#define CREATE_TRACE_POINTS
+#include <trace/events/alarmtimer.h>
+
 /**
  * struct alarm_base - Alarm timer bases
  * @lock:		Lock for syncrhonized access to the base
