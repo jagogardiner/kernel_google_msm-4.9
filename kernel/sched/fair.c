a8349c0a8385f1ff0a76e860576dd6338dc0c0b9
Todd Kjos <tkjos@google.com>
2018-06-20 15:38:28 -0700

    ANDROID: sched/fair: If sync flag ignored, try to place in same cluster
    If the sync flag is ignored because the current cpu is not in
    the affinity mask for the target of a sync wakeup (usually
    binder call), prefer to place in the same cluster if
    possible. The main case is a "top-app" task waking a
    "foreground" task when the top-app task is running on
    a CPU that is not in the foreground cpuset. This patch
    causes the "top-app" search order be used (big cpus first)
    even if schedtune.boost is 0 when the sync flag failed.
    
    Bug: 110464019
    Change-Id: Ib026231cee5c8b9d616d4ad4b92dd8502e79d0c0
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7981,7 +7981,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 			      && cpumask_test_cpu(cpu, tsk_cpus_allowed(p)));
 	}
 
-	if (energy_aware() && !(cpu_rq(prev_cpu)->rd->overutilized)) {
+	if (energy_aware()) {
+		rcu_read_lock();
 		/*
 		 * If the sync flag is set but ignored, prefer to
 		 * select cpu in the same cluster as current. So
@@ -7990,8 +7991,9 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		 * should be used.
 		 */
 		bool sync_boost = sync && cpu >= start_cpu(true);
-
-		return select_energy_cpu_brute(p, prev_cpu, sync_boost);
+		new_cpu = select_energy_cpu_brute(p, prev_cpu, sync_boost);
+		rcu_read_unlock();
+		return new_cpu;
 	}
 
 	rcu_read_lock();
