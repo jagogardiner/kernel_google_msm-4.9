2700ef3b91b8945ccc92646903bcbb19fd4f7d44
Santosh Mardi <gsantosh@codeaurora.org>
2018-11-28 14:59:28 +0530

    cpufreq: add CPUFREQ_INCOMPATIBLE event in policy notifiers
    Add support for CPUFREQ_INCOMPATIBLE event in policy notifiers
    this event is added for thermal driver to make sure thermal
    is the last client in applying the policy limits.
    
    Change-Id: I392e4745957829dd923f1f40201ad5c6b8ccf006
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>

--- include/linux/cpu_cooling.h
+++ include/linux/cpu_cooling.h
@@ -28,8 +28,8 @@
 #include <linux/thermal.h>
 #include <linux/cpumask.h>
 
-typedef int (*get_static_t)(cpumask_t *cpumask, int interval,
-			    unsigned long voltage, u32 *power);
+struct cpufreq_policy;
+
 typedef int (*plat_mitig_t)(int cpu, u32 clip_freq);
 
 struct cpu_cooling_ops {
@@ -39,51 +39,22 @@ struct cpu_cooling_ops {
 #ifdef CONFIG_CPU_THERMAL
 /**
  * cpufreq_cooling_register - function to create cpufreq cooling device.
- * @clip_cpus: cpumask of cpus where the frequency constraints will happen
+ * @policy: cpufreq policy.
  */
 struct thermal_cooling_device *
-cpufreq_cooling_register(const struct cpumask *clip_cpus);
-
-struct thermal_cooling_device *
-cpufreq_power_cooling_register(const struct cpumask *clip_cpus,
-			       u32 capacitance, get_static_t plat_static_func);
-
-struct thermal_cooling_device *
-cpufreq_platform_cooling_register(const struct cpumask *clip_cpus,
-					struct cpu_cooling_ops *ops);
+cpufreq_cooling_register(struct cpufreq_policy *policy);
 
 /**
- * of_cpufreq_cooling_register - create cpufreq cooling device based on DT.
- * @np: a valid struct device_node to the cooling device device tree node.
- * @clip_cpus: cpumask of cpus where the frequency constraints will happen
+ * cpufreq_platform_cooling_register - create cpufreq cooling device with
+ * additional platform specific mitigation function.
+ *
+ * @policy: cpufreq policy
+ * @plat_ops: the platform mitigation functions that will be called insted of
+ * cpufreq, if provided.
  */
-#ifdef CONFIG_THERMAL_OF
 struct thermal_cooling_device *
-of_cpufreq_cooling_register(struct device_node *np,
-			    const struct cpumask *clip_cpus);
-
-struct thermal_cooling_device *
-of_cpufreq_power_cooling_register(struct device_node *np,
-				  const struct cpumask *clip_cpus,
-				  u32 capacitance,
-				  get_static_t plat_static_func);
-#else
-static inline struct thermal_cooling_device *
-of_cpufreq_cooling_register(struct device_node *np,
-			    const struct cpumask *clip_cpus)
-{
-	return ERR_PTR(-ENOSYS);
-}
-
-static inline struct thermal_cooling_device *
-of_cpufreq_power_cooling_register(struct device_node *np,
-				  const struct cpumask *clip_cpus,
-				  u32 capacitance,
-				  get_static_t plat_static_func)
-{
-	return NULL;
-}
-#endif
+cpufreq_platform_cooling_register(struct cpufreq_policy *policy,
+					struct cpu_cooling_ops *ops);
 
 /**
  * cpufreq_cooling_unregister - function to remove cpufreq cooling device.
@@ -91,41 +62,11 @@ of_cpufreq_power_cooling_register(struct device_node *np,
  */
 void cpufreq_cooling_unregister(struct thermal_cooling_device *cdev);
 
-unsigned long cpufreq_cooling_get_level(unsigned int cpu, unsigned int freq);
 #else /* !CONFIG_CPU_THERMAL */
 static inline struct thermal_cooling_device *
-cpufreq_cooling_register(const struct cpumask *clip_cpus)
+cpufreq_cooling_register(struct cpufreq_policy *policy)
 {
 	return ERR_PTR(-ENOSYS);
-}
-static inline struct thermal_cooling_device *
-cpufreq_power_cooling_register(const struct cpumask *clip_cpus,
-			       u32 capacitance, get_static_t plat_static_func)
-{
-	return NULL;
-}
-
-static inline struct thermal_cooling_device *
-of_cpufreq_cooling_register(struct device_node *np,
-			    const struct cpumask *clip_cpus)
-{
-	return ERR_PTR(-ENOSYS);
-}
-
-static inline struct thermal_cooling_device *
-of_cpufreq_power_cooling_register(struct device_node *np,
-				  const struct cpumask *clip_cpus,
-				  u32 capacitance,
-				  get_static_t plat_static_func)
-{
-	return NULL;
-}
-
-static inline struct thermal_cooling_device *
-cpufreq_platform_cooling_register(const struct cpumask *clip_cpus,
-					struct cpu_cooling_ops *ops)
-{
-	return NULL;
 }
 
 static inline
@@ -133,11 +74,48 @@ void cpufreq_cooling_unregister(struct thermal_cooling_device *cdev)
 {
 	return;
 }
-static inline
-unsigned long cpufreq_cooling_get_level(unsigned int cpu, unsigned int freq)
-{
-	return THERMAL_CSTATE_INVALID;
-}
 #endif	/* CONFIG_CPU_THERMAL */
 
+#if defined(CONFIG_THERMAL_OF) && defined(CONFIG_CPU_THERMAL)
+/**
+ * of_cpufreq_cooling_register - create cpufreq cooling device based on DT.
+ * @policy: cpufreq policy.
+ */
+struct thermal_cooling_device *
+of_cpufreq_cooling_register(struct cpufreq_policy *policy);
+#else
+static inline struct thermal_cooling_device *
+of_cpufreq_cooling_register(struct cpufreq_policy *policy)
+{
+	return NULL;
+}
+
+static inline struct thermal_cooling_device *
+cpufreq_platform_cooling_register(struct cpufreq_policy *policy,
+					struct cpu_cooling_ops *ops)
+{
+	return NULL;
+}
+#endif /* defined(CONFIG_THERMAL_OF) && defined(CONFIG_CPU_THERMAL) */
+
+#if IS_ENABLED(CONFIG_QTI_CPU_ISOLATE_COOLING_DEVICE)
+extern void cpu_cooling_max_level_notifier_register(struct notifier_block *n);
+extern void cpu_cooling_max_level_notifier_unregister(struct notifier_block *n);
+extern const struct cpumask *cpu_cooling_get_max_level_cpumask(void);
+#else
+static inline
+void cpu_cooling_max_level_notifier_register(struct notifier_block *n)
+{
+}
+
+static inline
+void cpu_cooling_max_level_notifier_unregister(struct notifier_block *n)
+{
+}
+
+static inline const struct cpumask *cpu_cooling_get_max_level_cpumask(void)
+{
+	return cpu_none_mask;
+}
+#endif /* CONFIG_QTI_CPU_ISOLATE_COOLING_DEVICE */
 #endif /* __CPU_COOLING_H__ */
