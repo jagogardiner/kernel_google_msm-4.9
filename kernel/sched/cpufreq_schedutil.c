f2731af8543b8dcc834cd17619255b6737c38e8a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-27 12:48:41 -0700

    cpufreq: schedutil: Hard-code rate-limit thresholds
    Now that the CPU clock driver rate-limits CPU frequency updates to once
    every 10 ms, it doesn't make sense for schedutil's up rate-limit to be
    500 us, which is what userspace sets it to.
    
    With the new CPU frequency update restriction in mind, hard-code an up
    rate-limit of 10 ms and a down rate-limit of 20 ms (which is what
    userspace currently sets the down rate-limit to anyway).
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I29b744b303104bb71068cefbc11e784ca337c315

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -511,6 +511,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	/* Don't let userspace change this */
+	return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -530,6 +533,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	/* Don't let userspace change this */
+	return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -765,7 +771,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us =
 		cpufreq_policy_transition_delay_us(policy);
 
-	tunables->up_rate_limit_us = 500;
+	/* Hard-code some sane rate-limit values */
+	tunables->up_rate_limit_us = 10000;
 	tunables->down_rate_limit_us = 20000;
 
 	tunables->iowait_boost_enable = true;
