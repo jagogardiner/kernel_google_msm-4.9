4c81993c0cf20b2cd90302d28eac5840ee440284
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-05-22 15:10:31 +0530

    sched/core: Improve CPU hotplug latency for rcu_expedited=1 case
    The sched_cpu_deactivate()->synchronize_rcu_mult() is not taking
    expedited path even though rcu_expedited is enabled. So split
    the synchronize_rcu_mult() call into synchronize_sched() and
    synchronize_rcu() to improve the hotplug latency.
    
    Change-Id: Ic21fbf790e7ed2f314b4058a489cde7813897c1f
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8077,7 +8077,6 @@ int sched_cpu_deactivate(unsigned int cpu)
 	 *
 	 * Do sync before park smpboot threads to take care the rcu boost case.
 	 */
-
 #ifdef CONFIG_PREEMPT
 	synchronize_sched();
 #endif
