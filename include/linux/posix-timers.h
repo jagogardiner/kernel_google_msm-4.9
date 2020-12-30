9db2eea7ccaca01ad1dd5b43b75a44bea464be05
nysascape <jago@nysascape.tech>
2020-12-30 23:40:17 +0000

    posix-timers: Inline with 4.14
    Change-Id: Ia16665b5ae09aed97f225323b40445e28a418e78
    Signed-off-by: nysascape <jago@nysascape.tech>

--- include/linux/posix-timers.h
+++ include/linux/posix-timers.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _linux_POSIX_TIMERS_H
 #define _linux_POSIX_TIMERS_H
 
@@ -110,10 +111,7 @@ void posix_cpu_timers_exit_group(struct task_struct *task);
 void set_process_cpu_timer(struct task_struct *task, unsigned int clock_idx,
 			   u64 *newval, u64 *oldval);
 
-long clock_nanosleep_restart(struct restart_block *restart_block);
-
 void update_rlimit_cpu(struct task_struct *task, unsigned long rlim_new);
 
 void posixtimer_rearm(struct siginfo *info);
-
 #endif
